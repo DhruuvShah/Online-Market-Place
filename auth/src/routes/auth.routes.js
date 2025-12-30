8ceb9fcc427bfa25e3e191e9135f86b93d53e891
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-12-30 21:40:54 +0530

    feat(auth): initialize authentication module with user registration functionality
    - Added package.json for dependencies and scripts
    - Implemented server setup in server.js
    - Created Express app with cookie parser and routes in app.js
    - Developed user model with Mongoose in user.model.js
    - Established database connection logic in db.js
    - Added validation middleware for user registration in validator.middleware.js
    - Created authentication routes in auth.routes.js
    - Set up in-memory MongoDB for testing in setup.js
    - Implemented basic user registration controller in auth.controller.js

---
+++ auth/src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const validators = require("../middlewares/validator.middleware");
+const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+const router = express.Router();
+
+// POST /auth/register
+router.post("/register",
+);
+
+
+
+module.exports = router;
