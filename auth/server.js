15b2f19279898f76524a151fce133cb58d52014c
Dhruv271006011006 <dhruv27shah@gmail.com>
2026-01-31 11:51:05 +0530

    Auth Service Added

--- auth/server.js
+++ auth/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
-connectDB();
+connectDB()
 
 app.listen(3000, () => {
   console.log("Auth service is listening on port 3000");
