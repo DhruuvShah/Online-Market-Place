fd7028538076a0dbd480c7f2642ee56a43226e26
Dhruv271006011006 <dhruv27shah@gmail.com>
2026-02-13 20:16:01 +0530

    Still No Responce In Ai Buddy Service

--- ai-buddy/src/app.js
+++ ai-buddy/src/app.js
@@ -4,4 +4,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send({
+    message: "AI Service is running!",
+  });
+});
+
+
+
 module.exports = app;
--- ai-buddy/src/sockets/socket.server.js
+++ ai-buddy/src/sockets/socket.server.js
@@ -1,6 +1,7 @@
 const { Server } = require("socket.io");
 const jwt = require("jsonwebtoken");
 const cookie = require("cookie");
+const agent = require("../agent/agent");
 
 async function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
@@ -16,15 +17,41 @@ async function initSocketServer(httpServer) {
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
       socket.user = decoded;
+      socket.token = token;
+
       next();
-    } catch (error) {
+    } catch (err) {
       next(new Error("Invalid token"));
     }
   });
 
   io.on("connection", (socket) => {
-    console.log("A user connected");
+    console.log(socket.user, socket.token);
+
+    socket.on("message", async (data) => {
+      const agentResponse = await agent.invoke(
+        {
+          messages: [
+            {
+              role: "user",
+              content: data,
+            },
+          ],
+        },
+        {
+          metadata: {
+            token: socket.token,
+          },
+        },
+      );
+
+      const lastMessage =
+        agentResponse.messages[agentResponse.messages.length - 1];
+
+      socket.emit("message", lastMessage.content);
+    });
   });
 }
 
